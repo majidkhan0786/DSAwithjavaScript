e9ddc6a501d779563b9b7f54d15f2ff971d72408
majidkhan0786 <majdkhan0786@gmmail.com>
2023-03-15 22:07:45 +0530

    Intro Done

---
+++ BasicDsa/basicIntro.js
@@ -0,0 +1,34 @@
+// what is Data Structure
+// Data is Way to Organize data so that we can use this Data Efficiantly
+
+// What is Alogorithm 
+// Steps or a Process to arrange the Whole Data
+
+            //     Data Struture
+ 
+                    //    ||
+
+            // Primitive  => Integer , float , Character , Boolean 
+            // Non Primitive => Arrays , List , Files  
+
+            //      List
+   // Liner List                 Non-Linear List
+//     ||                             ||
+// Stack   Queue                Graphs     Trees
+
+// In Linear  we can perform the operation only Strting Poitn and End Point
+// In Non Linear  we can perform anywhere on tree or Graph 
+
+
+// Data Structure opeations 
+//  Traversing
+//  Insertion
+//  Deletion
+//  Searching
+//  Sorting
+//  Merging
+
+
+
+
+
