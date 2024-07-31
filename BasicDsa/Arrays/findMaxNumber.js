527bacb2bf5ff3f8e5e789ad544d9790b88819d7
majdkhan0786 <majdkhan0786@gmail.com>
2024-07-31 22:03:15 +0530

    max number in Array

---
+++ BasicDsa/Arrays/findMaxNumber.js
@@ -0,0 +1,35 @@
+// const arr = [2, 23, 32, 3, 2, 42, 42, 4, 44, 45];
+// const findLargestNumber = (arr) => {
+//   const sortedArray = arr?.sort((a, b) => a - b);
+//   let max = 0;
+//   for (let i = 0; i < sortedArray.length; i++) {
+//     console.log(arr[i]);
+//     if (max < arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+//   console.log(max, "max");
+// };
+
+// findLargestNumber(arr);
+
+// const arr = [2, 23, 32, 3, 2, 42, 42, 4, 44, 45];
+// const findLargestNumber = (arr) => {
+//   let max = 0;
+//   for (const num of arr) {
+//     if (num > max) {
+//       max = num;
+//     }
+//   }
+//   console.log(max, "max");
+// };
+
+// findLargestNumber(arr);
+
+// const arr = [2, 23, 32, 3, 2, 42, 42, 4, 44, 45];
+// const findLargestNumber = (arr) => {
+//   const max = arr.reduce((acc, curr) => (acc > curr ? acc : curr), 0);
+//   console.log(max, "max");
+// };
+
+// findLargestNumber(arr);
